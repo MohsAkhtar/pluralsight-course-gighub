d45b36d55d0a5eddafda58a9ac54a8074d949fa3
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-24 15:06:56 +0000

    Built api to record when a user is attending a gig.

---
+++ GigHub/Dtos/AttendanceDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GigHub.Dtos
+{
+    // DTO - Data Transfer Object, architectural pattern to send data across processes
+    public class AttendanceDto
+    {
+        public int GigId { get; set; }
+    }
+}
